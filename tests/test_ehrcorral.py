2dbd2d385e821cee9a8bc8414bfba71c8b4dbc06
Nikhil Haas <nikhil@nikhilhaas.com>
2015-10-03 19:26:34 -0700

    Add test case setUp to generate fake patient info

--- tests/test_ehrcorral.py
+++ tests/test_ehrcorral.py
@@ -8,15 +8,25 @@ test_ehrcorral
 Tests for `ehrcorral` module.
 """
 
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 import unittest
 
 from ehrcorral import ehrcorral
+from faker import Faker
+
+fake = Faker()
+fake.seed(8548)
 
 
-class TestEhrcorral(unittest.TestCase):
+class TestEHRcorral(unittest.TestCase):
 
     def setUp(self):
-        pass
+        profile_fields = ['name', 'birthdate', 'ssn', 'address']
+        self.herd = [fake.profile(fields=profile_fields) for n in xrange(100)]
 
     def test_something(self):
         pass
