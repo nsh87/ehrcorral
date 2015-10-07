aad6cb9b459748cdba5d8548c2b13e48316c637c
Nikhil Haas <nikhil@nikhilhaas.com>
2015-10-06 20:48:56 -0700

    Define 'private' _Record class and more fields for each Record   - _Record class has the same fields as the Record class but adds     Soundex, NYSIIS, etc.

--- ehrcorral/herd.py
+++ ehrcorral/herd.py
@@ -2,30 +2,50 @@
 """Contains core classes and functions for defining populations and acting upon
 them.
 """
-
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
 
 from collections import namedtuple
 
 
-class Record(namedtuple('Record', ['first_name',
-                                   'middle_name',
-                                   'last_name',
-                                   'suffix',
-                                   'gender',
-                                   'sex',
-                                   'address',
-                                   'ssn',
-                                   'id_number',
-                                   'blood_type',
-                                   'birthdate'])):
-    """An immutable representation of an electronic health record primarily
+RECORD_FIELDS = (
+   'first_name',
+   'middle_name',
+   'last_name',
+   'suffix',
+   'address',
+   'sex',
+   'gender',
+   'ssn',
+   'birthdate',
+   'blood_type',
+)
+
+_RECORD_FIELDS = (
+   'soundex',
+   'nysiis',
+   'metaphone',
+   'dmetaphone',
+)
+
+
+class Record(namedtuple('Record', RECORD_FIELDS)):
+    __slots__ = ()  # Prevent per-instance dictionaries to reduce memory
+
+
+class _Record(namedtuple('_Record', Record._fields + _RECORD_FIELDS)):
+    """An immutable representation of an electronic health record, primarily
     containing identifying patient information.
+
+    This is an internal class that is not indented to be used directly.
     """
-    __slots__ = ()  # Prevent per-instance dictionaries for low memory
+    __slots__ = ()  # Prevent per-instance dictionaries to reduce memory
 
 
 class Herd(object):
-    """A collection of :py:class:`.Record` with methods for interacting with
+    """A collection of :py:class:`._Record`s with methods for interacting with
     and linking records in the herd.
 
     You need:
@@ -35,6 +55,29 @@ class Herd(object):
         * convert to unicode/ascii?
     - validate that you only have certain field names in the incoming dict
     - parse names into first, last, prefix, suffix
+
+    need to make separate .csv's for each region
+    get male and female names and combine
+    only use the No. 1, No. 2, and No. 3 names
+    download whatever is in the first <a> (some names will be left out)
+    split by / and add to list
+    split by , and add to list
+    split by ( and remove whatever comes after the opening parenthesis
+              then convert everything to ASCII (i.e. need to remove accented
+                                                characters, umlaus)
+
+              when you generate a single file, you might make 1000 names and
+              make the population of names representative of the population of
+              ethnicities in the US. or just compare separately.
+
+              what do you do with names with JR...SR...I...II?
+
+    HAVE ABILITY to grow a herd, which doesn't mean adding to the tuple of
+    Records...have it be a way around memory limitations. That means that
+    instead of 'appending' to the tuple of Records, it immediately sends
+    the data to append to the ES database. It should therefore be that you
+    require a connection object in order to create a herd (maybe not).
+
     """
     def __init__(self, population=None):
         self._population = None  # Declaration for readability
@@ -64,8 +107,9 @@ class Herd(object):
     @population.setter
     def population(self, value):
         """Validate and set the herd's population."""
-        if not isinstance(value, tuple):
-            raise ValueError("Expected a tuple.")
-        if not all(isinstance(profile, dict) for profile in value):
-            raise ValueError("Expected a tuple of dictionaries.")
+        if not isinstance(value, (tuple, list)):
+            raise ValueError("Expected a tuple or list.")
+        #if not all(isinstance(profile, dict) for profile in value):
+        #    raise ValueError("Expected a tuple of dictionaries.")
+        
         self._population = value
