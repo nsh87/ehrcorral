95f213be0e6dc8a6077a94a9d7972cf5c1b073b2
catterbu <cbatterbury@dons.usfca.edu>
2015-11-04 21:05:57 -0800

    Initial measures commit

---
+++ ehrcorral/measures.py
@@ -0,0 +1,9 @@
+"""Contains functions for measures of similarity between records.
+"""
+
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
