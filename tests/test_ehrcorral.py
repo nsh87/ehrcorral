ead78fb2b5715a34b3bb281f17de1d65fd2b0216
catterbu <cbatterbury@dons.usfca.edu>
2015-11-09 14:09:09 -0800

    Add tests to check compress empty name returns empty list

--- tests/test_ehrcorral.py
+++ tests/test_ehrcorral.py
@@ -143,30 +143,39 @@ class TestPhonemicCompression(unittest.TestCase):
     def setUpClass(cls):
         cls.name = ['Jellyfish']
         cls.names = ['Jellyfish', 'Exeter']
+        cls.empty = ['']
 
     def test_soundex_compression(self):
         single_compression = compress(self.name, soundex)
         self.assertEqual(single_compression, ['J412'])
         multiple_compressions = compress(self.names, soundex)
         self.assertEqual(multiple_compressions, ['J412', 'E236'])
+        empty_compression = compress(self.empty, soundex)
+        self.assertEqual(empty_compression, [''])
 
     def test_nysiis_compression(self):
         single_compression = compress(self.name, nysiis)
         self.assertEqual(single_compression, ['JALYF'])
         multiple_compressions = compress(self.names, nysiis)
         self.assertEqual(multiple_compressions, ['JALYF', 'EXATAR'])
+        empty_compression = compress(self.empty, nysiis)
+        self.assertEqual(empty_compression, [''])
 
     def test_metaphone_compression(self):
         single_compression = compress(self.name, metaphone)
         self.assertEqual(single_compression, ['JLFX'])
         multiple_compressions = compress(self.names, metaphone)
         self.assertEqual(multiple_compressions, ['JLFX', 'EKSTR'])
+        empty_compression = compress(self.empty, metaphone)
+        self.assertEqual(empty_compression, [''])
 
     def test_dmetaphone_compression(self):
         single_compression = compress(self.name, dmetaphone)
         self.assertEqual(single_compression, ['JLFX', 'ALFX'])
         multiple_compressions = compress(self.names, dmetaphone)
         self.assertEqual(multiple_compressions, ['JLFX', 'ALFX', 'AKSTR'])
+        empty_compression = compress(self.empty, dmetaphone)
+        self.assertEqual(empty_compression, [''])
 
 
 class TestPhonemicBlocking(unittest.TestCase):
