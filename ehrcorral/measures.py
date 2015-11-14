76654581ebfcaa350e454497c0846af47eb40006
catterbu <cbatterbury@dons.usfca.edu>
2015-11-14 14:20:05 -0800

    Add conditionals for determining similarity values based on ox-link

--- ehrcorral/measures.py
+++ ehrcorral/measures.py
@@ -28,10 +28,11 @@ def record_similarity(herd,
         """
     # look into how to use weights properly since S in the table seems to
     # already account for some of this.
-    forename_similarity = get_forename_similarity(herd,
-                                                  [first_record, second_record],
-                                                  forename_method,
-                                                  "fore")
+    forename_similarity = \
+        get_forename_similarity(herd,
+                                [first_record, second_record],
+                                forename_method,
+                                "fore")
     mid_forename_similarity = \
         get_forename_similarity(herd,
                                 [first_record, second_record],
@@ -47,11 +48,21 @@ def get_forename_similarity(herd, records, method, type):
     forename_weight = max(first_weight, second_weight, 1.0 / 1000)
     forename_cutoff = 5.0 / 26
     difference = damerau_levenshtein(first_forename, second_forename)
+    max_length = max(len(first_forename), len(second_forename))
     # investigate use of weights
     if forename_weight > forename_cutoff:
         F = 3
     else:
         F = 12
+    if difference == 0:
+        similarity = 2 * F
+    elif float(difference) / max_length < 0.3:
+        similarity = F
+    elif float(difference) / max_length < 0.6:
+        similarity = -F
+    else:
+        similarity = -2 * F
+    return similarity
 
 
 def extract_forename_similarity_info(herd, record, type):
@@ -65,4 +76,185 @@ def extract_forename_similarity_info(herd, record, type):
                               record._meta.forename_freq_ref] / float(sum(
         herd._forename_freq_dict.values()))
     return forename, weight
+
+
+def get_surname_similarity(herd, records, method, name_type):
+    """Determine weights for the likelihood of two surnames being the same.
+
+    Args:
+        herd (Herd): An object of :py:class:`.Herd` which contains the two
+            records being compared.
+        records (List[Record]): A list of two objects of :py:class:`.Record`
+            to be compared to one another.
+        method (func): A function to be used to compare the surnames.
+        name_type (unicode): A unicode string to indicate which surname is
+            being compared.
+
+    Returns:
+        The surname weight for the similarity of the surnames.
+    """
+    name_types = ["birth", "current"]
+    first_surname, first_freq = \
+        extract_surname_similarity_info(herd, records[0], name_type)
+    # Get both names and frequencies from second record to compare to first
+    second_surname = [
+        extract_surname_similarity_info(herd, records[1], name)[0]
+        for name in name_types
+        ]
+    second_freq = [
+        extract_surname_similarity_info(herd, records[1], name)[1]
+        for name in name_types
+        ]
+    # Get difference between first record name and both second record names,
+    # then find the one that has the minimum difference and keep that one
+    diffs = [method(first_surname, name) for name in second_surname]
+    difference = min(diffs)
+    min_index = diffs.index(min(diffs))
+    second_surname = second_surname[min_index]
+    second_freq = second_freq[min_index]
+    max_length = max(len(first_surname), len(second_surname))
+    prop_diff = float(difference) / max_length
+    prop_freq = max(first_freq, second_freq, 1.0 / 1000)
+    cutoff = 1.0 / 500  # arbitrary, could be improved
+    S = 6 if prop_freq > cutoff else 17
+    # map prop_diff from (0, 1) to (-2, 2), then flip sign since lower diff
+    # implies that the two name are more similar.
+    weight = -(4 * prop_diff - 2)
+    return weight * S
+
+
+def extract_surname_similarity_info(herd, record, name_type):
+    """Extract desired surname and associated frequency weight.
+
+    Args:
+        herd (Herd): An object of :py:class:`.Herd` which contains the
+            frequency dictionary used for the frequency weight.
+        record (Record): An object of :py:class:`.Record` from which to
+            extract the surname.
+        name_type (unicode): A unicode string to indicate which surname is
+            being extracted.
+
+    Returns:
+        The forename and associated frequency weight for requested name.
+    """
+    profile = record.profile
+    # Add try/except
+    if name_type == "birth":
+        surname = profile.birth_surname
+        weight = herd._surname_freq_dict[record._meta.birth_surname_freq_ref]\
+            / float(sum(herd._surname_freq_dict.values()))
+    elif name_type == "current":
+        surname = profile.current_surname
+        weight = herd._surname_freq_dict[record._meta.current_surname_freq_ref]\
+            / float(sum(herd._surname_freq_dict.values()))
+    return surname, weight
+
+
+def get_address_similarity(records, method=damerau_levenshtein):
+    """Determine weights for the likelihood of two addresses being the same.
+
+    Args:
+        records (List[Record]): A list of two objects of :py:class:`.Record`
+            to be compared to one another.
+        method (func): A function to be used to compare the addresses.
+
+    Returns:
+        The address weight for the similarity of the addresses.
+    """
+    # ox-link only takes first 8 characters and greps for things like "flat"
+    first_profile = records[0].profile
+    second_profile = records[1].profile
+    first_address = [first_profile.address1, first_profile.address2]
+    second_address = [second_profile.address1, second_profile.address2]
+    diff1 = method(first_address[0], second_address[0])
+    diff2 = method(first_address[1], second_address[1])
+    if diff1 == 0:
+        if diff2 == 0:
+            return 7
+        else:
+            return 2
+    else:
+        return 0
+    # ox-link method
+    # return 7 if diff1 == 0 else 0
+
+
+def get_post_code_similarity(records, method=damerau_levenshtein):
+    """Determine weights for the likelihood of two postal codes being the same.
+
+    Args:
+        records (List[Record]): A list of two objects of :py:class:`.Record`
+            to be compared to one another.
+        method (func): A function to be used to compare the postal codes.
+
+    Returns:
+        The postal code weight for the similarity of the postal codes.
+    """
+    first_profile = records[0].profile
+    second_profile = records[1].profile
+    first_post_code = str(first_profile.postal_code)  # must be a string
+    second_post_code = str(second_profile.postal_code)  # must be a string
+    difference = method(first_post_code, second_post_code)
+    if difference == 0:
+        return 4
+    elif difference == 1:  # for transposition, ox-link does not do this
+        return 1
+    else:
+        return 0
+    # ox-link method
+    # return 4 if difference == 0 else 0
+
+
+def get_sex_similarity(records):
+    """Determine weights for the likelihood of two sexes being the same.
+
+    Args:
+        records (List[Record]): A list of two objects of :py:class:`.Record`
+            to be compared to one another.
+
+    Returns:
+        The sex weight for the similarity of the sexes.
+    """
+    # consider how better to account for sexes besides male and female
+    first_profile = records[0].profile
+    second_profile = records[1].profile
+    first_sex = str(first_profile.sex)  # must be a string
+    second_sex = str(second_profile.sex)  # must be a string
+    return 1 if first_sex == second_sex else -10
+
+
+def get_dob_similarity(records, method=damerau_levenshtein):
+    """Determine weights for the likelihood of two dates of birth being the
+    same.
+
+    Args:
+        records (List[Record]): A list of two objects of :py:class:`.Record`
+            to be compared to one another.
+        method (func): A function to be used to compare the dates of birth.
+
+    Returns:
+        The date of birth weight for the similarity of the dates of birth.
+    """
+    first_profile = records[0].profile
+    second_profile = records[1].profile
+    first_dob = str(first_profile.birth_year), \
+        str(first_profile.birth_month), \
+        str(first_profile.birth_day)
+    second_dob = str(second_profile.birth_year), \
+        str(second_profile.birth_month), \
+        str(second_profile.birth_day)
+    year_diff = method(first_dob[0], second_dob[0])
+    month_diff = method(first_dob[1], second_dob[1])
+    month_length = max(len(first_dob[1]), len(second_dob[1]))
+    day_diff = method(first_dob[2], second_dob[2])
+    # could add more complexity here based off of ox-link
+    year_prop = 0.5  # slightly arbitrary choice because year means more
+    month_prop = 0.25
+    day_prop = 0.25
+    prop_diff = year_prop * (year_diff / 4.0) + \
+        month_prop * (month_diff / float(month_length)) + \
+        day_prop * (day_diff / 2.0)
+    # map prop_diff from (0, 1) to (-23, 14), then flip sign since lower diff
+    # implies that the two name are more similar.
+    return -(37 * prop_diff - 14)
 
