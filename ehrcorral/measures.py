b00da0081289f8122d37729b874e135e29cbb124
catterbu <cbatterbury@dons.usfca.edu>
2015-11-14 10:24:23 -0800

    WIP Build out record_similarity function with sub-functions   - Add get_forename_similarity function that determines ox-link values   - Add extract_forename_similarity_info to get name and weight   - Start setting up what will become ox-link table

--- ehrcorral/measures.py
+++ ehrcorral/measures.py
@@ -26,17 +26,43 @@ def record_similarity(herd,
         surname_method (func): A function that performs some sort of
             comparison between strings.
         """
-    first_profile = first_record.profile
-    second_profile = second_record.profile
-    first_forename = first_profile.forename
-    second_forename = second_profile.forename
-    forename_weight = max(herd._forename_freq_dict[
-                              first_record._meta.forename_freq_ref],
-                          herd._forename_freq_dict[
-                              second_record._meta.forename_freq_ref]) / sum(
-        herd._forename_freq_dict.values())
-    forename_similarity = name_similarity([first_forename, second_forename],
-                                          forename_weight)
+    # look into how to use weights properly since S in the table seems to
+    # already account for some of this.
+    forename_similarity = get_forename_similarity(herd,
+                                                  [first_record, second_record],
+                                                  forename_method,
+                                                  "fore")
+    mid_forename_similarity = \
+        get_forename_similarity(herd,
+                                [first_record, second_record],
+                                forename_method,
+                                "mid_fore")
 
-def name_similarity(names, weights, method):
-    pass
+
+def get_forename_similarity(herd, records, method, type):
+    first_forename, first_weight = \
+        extract_forename_similarity_info(herd, records[0], type)
+    second_forename, second_weight = \
+        extract_forename_similarity_info(herd, records[1], type)
+    forename_weight = max(first_weight, second_weight, 1.0 / 1000)
+    forename_cutoff = 5.0 / 26
+    difference = damerau_levenshtein(first_forename, second_forename)
+    # investigate use of weights
+    if forename_weight > forename_cutoff:
+        F = 3
+    else:
+        F = 12
+
+
+def extract_forename_similarity_info(herd, record, type):
+    profile = record.profile
+    # Add try/except
+    if type == "fore":
+        forename = profile.forename
+    elif type == "mid_fore":
+        forename = profile.mid_forename
+    weight = herd._forename_freq_dict[
+                              record._meta.forename_freq_ref] / float(sum(
+        herd._forename_freq_dict.values()))
+    return forename, weight
+
