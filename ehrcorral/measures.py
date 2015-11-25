4d889ec684b5e2a3f0caac38a3875f28630ea788
catterbu <cbatterbury@dons.usfca.edu>
2015-11-24 22:12:08 -0800

    Remove unnecessary import of Herd and Record classes in measures

--- ehrcorral/measures.py
+++ ehrcorral/measures.py
@@ -6,8 +6,6 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import json
-import string
 from pylev import levenshtein, damerau_levenshtein
 
 
