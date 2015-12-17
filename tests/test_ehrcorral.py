315c4de0baaa4d925b8a15b6d6293189ef55810b
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-16 23:32:16 -0800

    Add test to print out similarity matrix for character transposition

--- tests/test_ehrcorral.py
+++ tests/test_ehrcorral.py
@@ -670,3 +670,19 @@ class TestCommonCharacterErrors(unittest.TestCase):
         herd.corral()
         print()
         print(herd._similarity_matrix)
+
+    def test_character_transposition(self):
+        """See how character transposition in someone's name impacts probability
+        matrix.
+        """
+        herd = Herd()
+        data_path = os.path.join(os.path.dirname(__file__),
+                                 'character_transposition_3x2.json')
+        with open(data_path, 'r') as data_file:
+            population = tuple(json.load(data_file))
+        records = [gen_record(profile) for profile in population]
+        herd = Herd()
+        herd.populate(records)
+        herd.corral()
+        print()
+        print(herd._similarity_matrix)
