76c84c00c1253c7512d6da74d1bc5794b0e8c452
Nikhil Haas <nikhil@nikhilhaas.com>
2015-12-17 01:10:21 -0800

    Update Travis config install Latex, remove python-numpy - You need Latex for showing Latex math in the docs. - You no longer need to apt-get install python-numpy since you're   using Conda on Travis.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.pngmath'
+    'sphinx.ext.pngmath',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
